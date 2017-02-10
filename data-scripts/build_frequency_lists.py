07b07687cb5bead74e10d38ba59fd58976b0547b
Alexander Inozemtsev <alexander.inozemtsev@gmail.com>
2017-02-10 13:43:31 +0300

    HH-66487 add runet frequent passwords

--- data-scripts/build_frequency_lists.py
+++ data-scripts/build_frequency_lists.py
@@ -29,12 +29,10 @@ passed data dir, or vice-versa.
 
 # maps dict name to num words. None value means "include all words"
 DICTIONARIES = dict(
-    us_tv_and_film    = 30000,
-    english_wikipedia = 30000,
-    passwords         = 30000,
-    surnames          = 10000,
-    male_names        = None,
-    female_names      = None,
+    mail              = None,
+    ya                = None,
+    rambler           = None,
+    vk                = None,
 )
 
 # returns {list_name: {token: rank}}, as tokens and ranks occur in each file.
